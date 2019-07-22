364a4126f818daa01c926395c92d3a3c9befe36b
nico <nicolas.raoux@gmail.com>
2019-07-22 11:30:32 +0200

    appel volley OK

--- app/src/main/java/com/example/whackahero/Helper.java
+++ app/src/main/java/com/example/whackahero/Helper.java
@@ -1,4 +1,62 @@
 package com.example.whackahero;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Helper {
+
+    public static void extractHero(Context context, final HeroListener listener) {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+
+        String url = "https://akabab.github.io/superhero-api/api/all.json";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                List<Hero> heroes = new ArrayList<>();
+                try {
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject heroObject = response.getJSONObject(i);
+                        JSONObject images = heroObject.getJSONObject("images");
+                        String url = images.getString("sm");
+                        Hero hero = new Hero(url);
+                        heroes.add(hero);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                listener.onHeroesLoaded(heroes);
+            }
+
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("VOLLEY_ERROR", "onErrorResponse: " + error.getMessage());
+                    }
+                }
+
+        );
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    public interface HeroListener {
+        void onHeroesLoaded(List<Hero> heroes);
+    }
 }
--- app/src/main/java/com/example/whackahero/MainActivity.java
+++ app/src/main/java/com/example/whackahero/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.whackahero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView test = findViewById(R.id.iv);
+
+        Helper.extractHero(MainActivity.this, new Helper.HeroListener() {
+            @Override
+            public void onHeroesLoaded(List<Hero> heroes) {
+                Hero hero = heroes.get(0);
+                String url = hero.getUrl();
+
+                Glide.with(MainActivity.this).load(url) .into(test);
+            }
+        });
+
+
+
     }
 }
