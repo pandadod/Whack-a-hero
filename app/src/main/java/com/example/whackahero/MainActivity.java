97d448d20d387f5880dda997cbe138e1c7747209
nico <nicolas.raoux@gmail.com>
2019-07-22 15:32:37 +0200

    click sur l'image et evolution du score fonctionnels

--- app/src/main/java/com/example/whackahero/MainActivity.java
+++ app/src/main/java/com/example/whackahero/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.whackahero;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -12,21 +14,119 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private List<Hero> heroesList = new ArrayList<>();
+    private ImageView iv1;
+    private ImageView iv2;
+    private ImageView iv3;
+    private ImageView iv4;
+    private ImageView iv5;
+    private ImageView iv6;
+    private ImageView iv7;
+    private ImageView iv8;
+    private ImageView ivAnswer;
+    private int answer;
+    private int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView tvScore = findViewById(R.id.tvScore);
+        ivAnswer = findViewById(R.id.ivAnswer);
+        iv1 = findViewById(R.id.iv1);
+        iv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = 0;
+                if (result == answer) {
+                    score++;
+                    tvScore.setText(String.valueOf(score));
+                }
+            }
+        });
 
-        final ImageView iv1 = findViewById(R.id.iv1);
-        final ImageView iv2 = findViewById(R.id.iv2);
-        final ImageView iv3 = findViewById(R.id.iv3);
-        final ImageView iv4 = findViewById(R.id.iv4);
-        final ImageView iv5 = findViewById(R.id.iv5);
-        final ImageView iv6 = findViewById(R.id.iv6);
-        final ImageView iv7 = findViewById(R.id.iv7);
-        final ImageView iv8 = findViewById(R.id.iv8);
-        final ImageView ivAnswer = findViewById(R.id.ivAnswer);
+        iv2 = findViewById(R.id.iv2);
+        iv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = 1;
+                if (result == answer) {
+                    score++;
+                    tvScore.setText(String.valueOf(score));
+                }
+            }
+        });
+
+        iv3 = findViewById(R.id.iv3);
+        iv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = 2;
+                if (result == answer) {
+                    score++;
+                    tvScore.setText(String.valueOf(score));
+                }
+            }
+        });
+
+        iv4 = findViewById(R.id.iv4);
+        iv4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = 3;
+                if (result == answer) {
+                    score++;
+                    tvScore.setText(String.valueOf(score));
+                }
+            }
+        });
+
+        iv5 = findViewById(R.id.iv5);
+        iv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = 4;
+                if (result == answer) {
+                    score++;
+                    tvScore.setText(String.valueOf(score));
+                }
+            }
+        });
+
+        iv6 = findViewById(R.id.iv6);
+        iv6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = 5;
+                if (result == answer) {
+                    score++;
+                    tvScore.setText(String.valueOf(score));
+                }
+            }
+        });
+
+        iv7 = findViewById(R.id.iv7);
+        iv7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = 6;
+                if (result == answer) {
+                    score++;
+                    tvScore.setText(String.valueOf(score));
+                }
+            }
+        });
+
+        iv8 = findViewById(R.id.iv8);
+        iv8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int result = 7;
+                if (result == answer) {
+                    score++;
+                    tvScore.setText(String.valueOf(score));
+                }
+            }
+        });
 
         Helper.extractHero(MainActivity.this, new Helper.HeroListener() {
             @Override
@@ -68,10 +168,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setImage(List<Hero> heroesList, ImageView ivAnswer) {
+
         Random r = new Random();
         int index = r.nextInt((7 - 0) + 1) + 0;
         Hero heroAnswer = heroesList.get(index);
         String urlAnswer = heroAnswer.getUrl();
+        answer = index;
         Glide.with(MainActivity.this).load(urlAnswer).into(ivAnswer);
     }
 }
