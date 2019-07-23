ac2533f6f17d8ea966e8745af26ba94bb4b45446
nico <nicolas.raoux@gmail.com>
2019-07-23 05:21:23 +0200

    mis à jour score max OK

--- app/src/main/java/com/example/whackahero/ConnexionActivity.java
+++ app/src/main/java/com/example/whackahero/ConnexionActivity.java
@@ -1,15 +1,13 @@
 package com.example.whackahero;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.util.Consumer;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.nio.charset.Charset;
 
 public class ConnexionActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/whackahero/ScoreActivity.java
+++ app/src/main/java/com/example/whackahero/ScoreActivity.java
@@ -2,6 +2,7 @@ package com.example.whackahero;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Consumer;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        User user = UserSingleton.getInstance().getUser();
 
         Intent intent = getIntent();
         int score = intent.getIntExtra("score", -1);
@@ -21,17 +23,27 @@ public class ScoreActivity extends AppCompatActivity {
         TextView tvAccuracy = findViewById(R.id.tvAccuracy);
         tvAccuracy.setText("Your accuracy is : " + score * 100 / accuracy + "%");
         TextView tvPhrase = findViewById(R.id.tvPhrase);
-        if (score<20) {
+        if (score < 20) {
             tvPhrase.setText("Thanks for playing but this game is not made for you, obviously !");
         }
-        if (score>=20 && score<40) {
+        if (score >= 20 && score < 40) {
             tvPhrase.setText("Not bad ! Feel free to purchase the app on Google Play Store to try again");
         }
-        if (score>=40 && score<=60) {
+        if (score >= 40 && score <= 60) {
             tvPhrase.setText("Well done ! Yet you still have a long way to beat our master guru Bastien !");
         }
-        if (score>60) {
+        if (score > 60) {
             tvPhrase.setText("Congratulations, but princess Peach is in another castle, try again !");
+        }
+
+        if (score > user.getScoreMax()) {
+            user.setScoreMax(score);
+            SingletonVolley.getInstance(ScoreActivity.this).modifyUser(user, new Consumer<User>() {
+                @Override
+                public void accept(User user) {
+                    UserSingleton.getInstance().setUser(user);
+                }
+            });
         }
     }
 }
