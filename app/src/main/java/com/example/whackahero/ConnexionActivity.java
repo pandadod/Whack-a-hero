8e6dd9a6410d0f341e57ee792d073d596e7e9b83
nico <nicolas.raoux@gmail.com>
2019-07-23 05:13:11 +0200

    ajout creation compte et connexion compte

--- app/src/main/java/com/example/whackahero/ConnexionActivity.java
+++ app/src/main/java/com/example/whackahero/ConnexionActivity.java
@@ -1,7 +1,15 @@
 package com.example.whackahero;
 
+import android.content.Intent;
+import android.support.v4.util.Consumer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.nio.charset.Charset;
 
 public class ConnexionActivity extends AppCompatActivity {
 
@@ -9,5 +17,64 @@ public class ConnexionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        Button btLogin = findViewById(R.id.btSignIn);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etLogin = findViewById(R.id.etLogin);
+                String login = etLogin.getText().toString();
+                EditText etPassword = findViewById(R.id.etPassword);
+                String password = etPassword.getText().toString();
+                User user = new User();
+                user.setName(login);
+                user.setPassword(password);
+                SingletonVolley.getInstance(ConnexionActivity.this).connexion(user, new Consumer<User>() {
+                    @Override
+                    public void accept(User user) {
+                        if (user != null) {
+                            UserSingleton.getInstance().initiateUser(
+                                    user.getId(), user.getName(),
+                                    user.getPassword(),
+                                    user.getScoreMax());
+                            UserSingleton.getInstance().getUser();
+
+                            Intent goToMainActivity = new Intent(ConnexionActivity.this, MainActivity.class);
+                            startActivity(goToMainActivity);
+
+                        } else {
+                            Toast.makeText(ConnexionActivity.this, "erreur", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        });
+        Button btSignUp = findViewById(R.id.btSignUp);
+        btSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etLoginCreate = findViewById(R.id.etLoginCreate);
+                String loginCreate = etLoginCreate.getText().toString();
+                EditText etPasswordSignUp = findViewById(R.id.etPasswordSignUp);
+                String passwordSignUp = etPasswordSignUp.getText().toString();
+
+                if (loginCreate.isEmpty() || passwordSignUp.isEmpty()) {
+                    Toast.makeText(ConnexionActivity.this, "not empty allowed", Toast.LENGTH_SHORT).show();
+                } else {
+                    User user = new User();
+                    user.setName(loginCreate);
+                    user.setPassword(passwordSignUp);
+                    SingletonVolley.getInstance(ConnexionActivity.this).createAccount(user, new Consumer<User>() {
+                        @Override
+                        public void accept(User user) {
+                            UserSingleton.getInstance().setUser(user);
+                            Intent intent = new Intent(ConnexionActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
